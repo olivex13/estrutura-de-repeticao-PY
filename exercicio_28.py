7a742a44fa07b0aebb9231489af53e3116705ff9
Olivex13 <silva.o.douglas@gmail.com>
2024-01-18 14:34:21 -0300

    correção exc_28

--- exercicio_28.py
+++ exercicio_28.py
@@ -1,4 +1,5 @@
-'''28 - Faça um programa que calcule o valor total investido por um colecionador em sua coleção de CDs e o valor médio gasto em cada um deles. O usuário deverá informar a quantidade de CDs e o valor para em cada um.'''
+'''28 - Faça um programa que calcule o valor total investido por um colecionador em sua coleção de CDs e o valor médio gasto em cada um deles. 
+O usuário deverá informar a quantidade de CDs e o valor para em cada um.'''
 
 
 print("COLEÇÃO DE CDs".center(120))
@@ -18,5 +19,17 @@ def calcular_media(valor_do_cd, contador):
  return media
 
 media = calcular_media(valor_do_cd, contador)
+total = sum(valor_do_cd)
 
-print(media)
+print('')
+print('Lista de CDs')
+
+if len(nome_do_cd) == len(valor_do_cd):
+  
+    for i in range(len(nome_do_cd)):
+        print(f"{nome_do_cd[i]} - {valor_do_cd[i]}")
+else:
+    print("As listas não têm o mesmo comprimento.")
+
+print(f'O valor total investido na coleção foi R$ {total:.2f}')
+print(f'O valor médio investido por CD é R$ {media:.2f}')
