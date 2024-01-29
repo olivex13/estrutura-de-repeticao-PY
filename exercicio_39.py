ec9bb3b88980c85950d329312eb5b80dd4c00c52
Olivex13 <silva.o.douglas@gmail.com>
2024-01-29 14:24:49 -0300

    exerc_39 finalizado

--- exercicio_39.py
+++ exercicio_39.py
@@ -15,21 +15,22 @@ contador = 0
 while contador < 10:
     aluno = int(input('Digite o numero do aluno: '))
     numr_aluno.append(aluno)
-    altura = float(input('Digite a altura do aluno: '))
+    altura = int(input('Digite a altura do aluno em centimentros: '))
     altura_aluno.append(altura)
     contador += 1
 
 aluno_mais_alto = altura_aluno[0]
 aluno_mais_baixo = altura_aluno[0]
 
-for i in range(altura_aluno):
-    if i >= aluno_mais_alto:
-        aluno_mais_alto = i
-    elif i <= aluno_mais_baixo:
-        aluno_mais_baixo = i
+for i in range(len(altura_aluno)):
+    if altura_aluno[i] > aluno_mais_alto:
+        aluno_mais_alto = altura_aluno[i]
+    elif altura_aluno[i] < aluno_mais_baixo:
+        aluno_mais_baixo = altura_aluno[i]
 
 indice_menor_altura = altura_aluno.index(min(altura_aluno))
 indice_maior_altura = altura_aluno.index(max(altura_aluno))
 
-print (f'''O aluno mais alto é
-''')
+print('')
+print (f'''O aluno mais alto é o de numero {numr_aluno[indice_maior_altura]} com {altura_aluno[indice_maior_altura]} centimetros. \n
+O  aluno mais baixo é o de numero {numr_aluno[indice_menor_altura]} com {altura_aluno[indice_menor_altura]} centimetros.''')
